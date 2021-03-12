d9ca8811b18b3f8ecdad48717041d8554cf2ac53
Daniil Yurchin <yurchin2398@gmail.com>
2021-03-12 16:02:06 +0200

    Terraform Build v.1.02

--- add_ec2_secgroup_jenkins.tf
+++ add_ec2_secgroup_jenkins.tf
@@ -21,12 +21,28 @@ resource "aws_instance" "homework" {                                  # Amazon I
   instance_type             =   "t2.micro"                            # Tariff type
   vpc_security_group_ids    =   [aws_security_group.homework.id]      # Dependenc Instance and Security Group
   user_data                 =   file("jenkinsInstall.sh")             # Script for Jenkins extension 
-  key_name                  =   "main_ssh"			      # SSH KEY
-
+  key_name                  =   "main_ssh"			                      # SSH KEY
+  connection {                                                        # SSH connections to my Instance
+    type        = "ssh" 
+    user        = "ubuntu"
+    private_key = file("~/project/studying/COURSE/secret/main_ssh")
+    host        = self.public_ip                                      # Instance Public IP
+  }
 tags = {
  Name = "Web Server Build by Terraform"
  Owner = "Daniil Yurchin"
-}
+  }
+provisioner "file" {                                                  # Copies "jenkinsInstall.sh" to folder tmp
+   source      = "jenkinsInstall.sh"                                  
+   destination = "/tmp/jenkinsInstall.sh"
+  }
+
+provisioner "remote-exec" {                                           # Invoke a script on a remote resource after it is created
+    inline = [                                                        # Tag for call jenkinsInstall.sh
+      "chmod +x /tmp/jenkinsInstall.sh",                              
+      "sudo /tmp/jenkinsInstall.sh",
+    ]
+  }
 }
 
 
@@ -38,25 +54,25 @@ resource "aws_security_group" "homework" {                            # Amazon S
     from_port   = 8080                                                # Allow connection from port 8080 
     to_port     = 8080                                                # Allow connection to port 8080  
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]                                       # Allow all connection
+    cidr_blocks = ["0.0.0.0/0"]                                       # Allow all IP connection
   }
 
-  ingress {                                                           # IN 
+  ingress {                                                           # IN Connection
     from_port   = 22                                                  # Allow connection from port 22 
     to_port     = 22                                                  # Allow connection to port 22  
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]                                       # Allow all connection
+    cidr_blocks = ["0.0.0.0/0"]                                       # Allow all IP connection
   }
 
-  egress {                                                            # OUT
-    from_port   = 0                                                   # ALL OUT
-    to_port     = 0                                                   # ALL OUT
-    protocol    = "-1"                                                # ALL OUT
-    cidr_blocks = ["0.0.0.0/0"]                                       # ALL OUT
+  egress {                                                            # OUT Connection
+    from_port   = 0                                                   
+    to_port     = 0                                                   
+    protocol    = "-1"                                                
+    cidr_blocks = ["0.0.0.0/0"]                                       
   }
 
   tags = {
  Name = "Web Server Build by Terraform"
  Owner = "Daniil Yurchin"
-}
+  }
 }
--- jenkinsInstall.sh
+++ jenkinsInstall.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-
+apt-get install  openjdk-11-jdk -y
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
+sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
     /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update
-sudo apt-get install jenkins openjdk-11-jdk -y
-sudo reboot
+apt-get update -y
+apt-get install jenkins -y
+echo "Public IP:"
+curl ifconfig.co
+echo "Password to Jenkins: "
+cat /var/lib/jenkins/secrets/initialAdminPassword
