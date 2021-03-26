06cfaf87ce24f090851876f8a22f3540203b0660
Daniil Yurchin <yurchin2398@gmail.com>
2021-03-26 10:34:59 +0200

    minor fix

--- jenkinsInstall.sh
+++ jenkinsInstall.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-apt-get install  openjdk-11-jdk -y
+apt-get update -y
+apt-get install openjdk-11-jdk -y
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
     /etc/apt/sources.list.d/jenkins.list'
 apt-get update -y
 apt-get install jenkins -y
-echo "Public IP:"
-curl ifconfig.co
 echo "Password to Jenkins: "
 cat /var/lib/jenkins/secrets/initialAdminPassword
