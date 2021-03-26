5bd369219d475099f4f49767ccf316e3fdaf4d05
Daniil Yurchin <yurchin2398@gmail.com>
2021-03-26 11:26:02 +0200

    fix 0.1:)

--- jenkinsInstall.sh
+++ jenkinsInstall.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-apt-get update -y
 apt-get install openjdk-11-jdk -y
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
